2d9ac1e47c389783a2c6b6656dd3cc43f9e7cfac
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-11-05 20:58:41 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# 環境構築
+
+- node: 6.11.1
+
+- npm: 3.10.10
+
+```
+cd BrowserIconSortAnimation
+npm install
+```
+
+` npm run serve ` だけでは上手く行かないので、2つのターミナルを開いて以下のコマンドを打ってください
+
+```
+npm run serve
+```
+
+```
+npm run watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+var path=require("path");
+var webpack=require("webpack");
+
+module.exports = {
+  entry: {
+    index: __dirname+'/src/js/index.js'
+  },
+  output: {
+    path: __dirname+"/dist/js",
+    filename: '[name].bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
+  devServer: {
+    contentBase: __dirname+"/dist",
+    port: 8080
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/, 
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query:{
+          presets: ["es2015"]
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ["style-loader","css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        loader: ["style-loader","css-loader","sass-loader"]
+      },
+      { 
+        test: /\.svg$/, 
+        loader: 'url-loader?mimetype=image/svg+xml'
+      },
+      { 
+        test: /\.woff$/, 
+        loader: 'url-loader?mimetype=application/font-woff' 
+      },
+      { 
+        test: /\.woff2$/, 
+        loader: 'url-loader?mimetype=application/font-woff' 
+      },
+      { 
+        test: /\.eot$/, 
+        loader: 'url-loader?mimetype=application/font-woff' 
+      },
+      { 
+        test: /\.ttf$/, 
+        loader: 'url-loader?mimetype=application/font-woff' 
+      },
+      { 
+        test: /\.png$/, 
+        loader: 'url-loader?mimetype=image/png' 
+      }
+    ]
+  }
+};
