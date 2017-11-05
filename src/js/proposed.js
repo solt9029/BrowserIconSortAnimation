114df3dfdf659251b0728a13c8bbceb56d7f8d93
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-11-06 08:32:44 +0900

    append cards

--- src/js/proposed.js
+++ src/js/proposed.js
@@ -4,5 +4,19 @@ require("bootstrap");
 import '../scss/common.scss';
 
 $(() => {
-    
+    const query = getQuery();
+    for (let i = 1; i <= query['num']; i++) {
+        $('body').append(`<div class="card" id="card${i}">${i}</div>`);
+    }
 });
+
+function getQuery() {
+    if(window.location.search === "") return;
+    const variables = window.location.search.split("?")[1].split("&");
+    const obj = {};
+    variables.forEach(function(v, i) {
+        const variable = v.split("=");
+        obj[variable[0]] = Number(variable[1]);
+    });
+    return obj;
+}
