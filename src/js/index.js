1b0c3923a5a9cd3a14aedf5d1ce2f85df68a93c9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-11-10 11:07:42 +0900

    link

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,9 @@ require("bootstrap");
 
 $(() => {
     $('#proposed').click(() => {
-        location.href = '/proposed.html?num=' + $('#num').val(); 
+        location.href = './proposed.html?num=' + $('#num').val(); 
     });
     $('#reflow').click(() => {
-        location.href = '/reflow.html?num=' + $('#num').val();
+        location.href = './reflow.html?num=' + $('#num').val();
     })
 });
