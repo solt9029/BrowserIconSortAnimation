1aa3e85c1e6e7f4a3da2cc8525c1c55ded578ead
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-11-10 14:50:23 +0900

    util file created

---
+++ src/js/util.js
@@ -0,0 +1,14 @@
+export function newTwoDimensionalArray(x, y) {
+    return Array.from(new Array(y), () => new Array(x).fill(0));
+}
+
+export function getQuery() {
+    if(window.location.search === "") return;
+    const variables = window.location.search.split("?")[1].split("&");
+    const obj = {};
+    variables.forEach(function(v, i) {
+        const variable = v.split("=");
+        obj[variable[0]] = Number(variable[1]);
+    });
+    return obj;
+}
