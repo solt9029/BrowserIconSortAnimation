330330d96802afaf3f6e93ec41305735d83bd261
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-03-25 23:04:13 +0900

    size,spaceをかへんにした

--- src/js/hybrid.js
+++ src/js/hybrid.js
@@ -2,11 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 require("bootstrap");
 import '../scss/hybrid.scss';
 import * as util from './util';
-import { SIZE, SPACE } from "./const";
 
 const query = util.getQuery();
 let patterns = util.newTwoDimensionalArray(query['num'], query['num']);
 let prevXNum;
+let size = 180;
+let space = 20;
 
 $(() => {
     for (let i = 0; i < query['num']; i++) {
@@ -33,7 +34,7 @@ $(window).on('resize', function() {
 });
 
 function proposed(startXNum) {
-    let endXNum = Math.floor((window.innerWidth - SPACE) / (SIZE + SPACE));
+    let endXNum = Math.floor((window.innerWidth - space) / (size + space));
     if (endXNum > query['num']) {
         endXNum = query['num'];
     }
@@ -50,8 +51,8 @@ function proposed(startXNum) {
         for (let y = 0; y < Math.ceil(query['num'] / (xNum + 1)); y++) {
             for (let x = 0; x < xNum + 1; x++) {
                 $(`#card${patterns[xNum][x + y * (xNum + 1)]}`).animate({
-                    'top': (y * (SIZE + SPACE) + SPACE) + 'px',
-                    'left': (x * (SIZE + SPACE) + SPACE) + 'px'
+                    'top': (y * (size + space) + space) + 'px',
+                    'left': (x * (size + space) + space) + 'px'
                 }, query['animation']);
             }
         }
@@ -61,8 +62,8 @@ function proposed(startXNum) {
         for (let y = 0; y < Math.ceil(query['num'] / (xNum - 1)); y++) {
             for (let x = 0; x < xNum - 1; x++) {
                 $(`#card${patterns[xNum - 2][x + y * (xNum - 1)]}`).animate({
-                    'top': (y * (SIZE + SPACE) + SPACE) + 'px',
-                    'left': (x * (SIZE + SPACE) + SPACE) + 'px'
+                    'top': (y * (size + space) + space) + 'px',
+                    'left': (x * (size + space) + space) + 'px'
                 }, query['animation']);
             }
         }
