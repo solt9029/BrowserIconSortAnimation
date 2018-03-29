e89831e3d4877cdeca0e299947d8a4050a3801fc
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-03-29 10:36:06 +0900

    xnumをproposedにも追加

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ require("bootstrap");
 
 $(() => {
     $('#proposed').click(() => {
-        location.href = './proposed.html?step=0&animation=500&num=' + $('#num').val(); 
+        location.href = './proposed.html?step=0&animation=500&xnum=4&num=' + $('#num').val(); 
     });
     $('#reflow').click(() => {
         location.href = './reflow.html?step=0&animation=500&num=' + $('#num').val();
