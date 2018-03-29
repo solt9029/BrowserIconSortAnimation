1d57e1dff799623edce283aa283a82591394ff0d
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-03-29 11:35:12 +0900

    スケーリングにステップはいらない

--- src/js/class/Scaling.js
+++ src/js/class/Scaling.js
@@ -19,10 +19,7 @@ export default class Scaling extends Method {
     animateCards() {
         for (let y = 0; y < Math.ceil(this.query['num'] / this.query['xnum']); y++) {
             for (let x = 0; x < this.query['xnum']; x++) {
-                if (!this.query['step']) {
-                    $(`#card${x + y * this.query['xnum']}`).stop();
-                }
-                $(`#card${x + y * this.query['xnum']}`).animate({
+                $(`#card${x + y * this.query['xnum']}`).stop().animate({
                     'top': (y * (this.size + this.space) + this.space) + 'px',
                     'left': (x * (this.size + this.space) + this.space) + 'px',
                     'width': this.size + 'px',
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ $(() => {
         location.href = './reflow.html?step=0&animation=500&num=' + $('#num').val();
     });
     $('#scaling').click(() => {
-        location.href = './scaling.html?step=0&animation=500&xnum=4&num=' + $('#num').val();
+        location.href = './scaling.html?animation=500&xnum=4&num=' + $('#num').val();
     });
     $('#hybrid').click(() => {
         location.href = './hybrid.html?step=0&animation=500&xnum=4&num=' + $('#num').val();
