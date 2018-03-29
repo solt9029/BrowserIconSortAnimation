d5a8e4aaad5e87d5ad08d6021a8946ab69702ec2
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-03-29 20:59:56 +0900

    概略を書いた

--- src/js/class/Hybrid.js
+++ src/js/class/Hybrid.js
@@ -17,9 +17,29 @@ export default class Hybrid extends Method {
         const min_size = Hybrid.MIN_SIZE;
         const min_space = Hybrid.MIN_SIZE * Hybrid.RATIO;
         let currentXNum = this.getXNum(min_size, min_space);
+        let currentYNum = this.getYNum(min_size, min_space);
+        
+        if (currentXNum >= this.query['xnum'] && currentYNum >= this.baseYNum) {
+            // スケーリング
+        } else if (currentXNum < this.query['xnum']) {
+            // 提案手法
+        } else if (currentYNum < this.baseYNum) {
+            // forを利用した提案手法
+        }
     }
 
     animateCards() {
 
+    }
+
+    getYNum(size, space) {
+        let yNum = 1;
+        if (Math.floor((window.innerHeight - space) / (space + size)) > 0) {
+            yNum = Math.floor((window.innerHeight - space) / (space + size));
+        }
+        if (yNum > this.query['num']) {
+            yNum = this.query['num'];
+        }
+        return yNum;
     }
 }
