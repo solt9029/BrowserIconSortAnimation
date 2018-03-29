0f9e81ab2ee1ebfe4f9ad10418be2a4ca2b35ffc
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-03-29 20:47:01 +0900

    hybrid途中まで

--- src/js/class/Hybrid.js
+++ src/js/class/Hybrid.js
@@ -1,12 +1,25 @@
 import Method from './Method';
 
 export default class Hybrid extends Method {
+    static get MIN_SIZE() {
+        return 90;
+    }
+
     constructor() {
         super();
-        this.hybrid();
-        let hybrid = this;
-        $(window).on('resize', function() {
-            hybrid.hybrid();
-        });
+        this.baseYNum = Math.ceil(this.query['num'] / this.query['xnum']);
+        this.prevXNum = 0;
+        this.prevYNum = 0;
+        this.init();
+    }
+
+    method() {
+        const min_size = Hybrid.MIN_SIZE;
+        const min_space = Hybrid.MIN_SIZE * Hybrid.RATIO;
+        let currentXNum = this.getXNum(min_size, min_space);
+    }
+
+    animateCards() {
+
     }
 }
