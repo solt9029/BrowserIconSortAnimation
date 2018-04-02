2766f2216930e1112dd488268731da2e98b8f21f
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-04-02 21:37:30 +0900

    要らない行消した

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 $(() => {
-    const query = 'ratio=0.1&default_size=180&min_size=90&step=0&animation=500&xnum=4&num=' + $('#num').val();
     $('button').on('click', function() {
         const path = './' + $(this).attr('id') + '.html';
         const query = 
